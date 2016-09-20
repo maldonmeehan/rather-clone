85d3d8dbb472e14e95bdde9a0cbef1d29e71d3fb
Cory Olson and Maldon Meehan <cory+maldon@>
2016-09-20 10:16:53 -0700

    add voting with ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,17 @@
 class VotesController < ApplicationController
+
+  def create
+    @current_user = User.find(params[:current_user])
+    @answer = Answer.find(params[:answer_id])
+    vote = Vote.new
+    vote.profile = current_user.profile
+    vote.answer_id = @answer.id
+    if vote.save!
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
+    end
+  end
+
 end
