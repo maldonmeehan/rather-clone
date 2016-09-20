1b0f330cf3f2b1174a29e4fe4421a889028a1f5f
Cory Olson and Maldon Meehan <maldon@>
2016-09-20 14:37:36 -0700

    add test coverage for voting

--- spec/features/votes_controller_spec.rb
+++ spec/features/votes_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe VotesController, type: :controller do
+describe "tests the vote process" do
+  it "will add a vote to the first item" do
+    user = FactoryGirl.create(:user)
+    profile = FactoryGirl.create(:profile, user: user)
+    question = FactoryGirl.create(:question, profile: profile)
+    answer1 = FactoryGirl.create(:answer)
+    answer2 = FactoryGirl.create(:answer)
+    question.answers = [answer1, answer2]
+    question.save
 
+    vote = FactoryGirl.create(:vote, profile: profile, answer: answer1)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit questions_path
+
+    click_button "vote-id-#{answer1.id}"
+  end
+
+  it "will add a vote to the second item" do
+
+  end
 end
