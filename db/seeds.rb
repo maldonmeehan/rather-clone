df6e37edec7b321bace9c8ba677c2b6f5dd1e4bc
Cory Olson and Maldon Meehan <cory+maldon@>
2016-09-20 09:16:01 -0700

    seed votes table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 20.times do | i |
+  # Generate a user
   user = User.new
   user.email = Faker::Internet.email
   user.password = "test_password"
   user.password_confirmation = "test_password"
   user.save!
-
+  # generate a profile
   profile = Profile.new
   profile.name = Faker::Name.name
   profile.country = Faker::Address.country
   profile.gender = Faker::StarWars.specie
   profile.user = user
   profile.save!
+
+  # generate answers
   answer1 = Answer.new(content: Faker::Hipster.sentence(3, false, 4))
   answer2 = Answer.new(content: Faker::Hipster.sentence(3, false, 4))
   question = Question.create(profile: profile, answers: [answer1, answer2])
+
+  vote = Vote.new
+  vote.profile = profile
+  vote.answer = answer1
+  vote.save!
 end
