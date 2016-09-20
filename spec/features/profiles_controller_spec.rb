f192373d7bba882ff74bb16f1e87c058608ffd1a
Cory Olson and Maldon Meehan <cory+maldon@>
2016-09-20 15:30:56 -0700

    add passing intergration spec for providing no content for profile and for visiting another users profile

--- spec/features/profiles_controller_spec.rb
+++ spec/features/profiles_controller_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe ProfilesController, type: :controller do
+describe "tests the profile process" do
+  it "will add a profile to a user" do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit new_profile_path
+    fill_in "Name", :with => "Test Name"
+    fill_in "Country", :with => "Test Country"
+    fill_in "Gender", :with => "Test Gender"
+    click_button "Create Profile"
+    click_on "Test Name"
+    expect(page).to have_content "Test Country"
+  end
 
+  it "will give an error when no content is provided by the user" do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit new_profile_path
+    fill_in "Name", :with => ""
+    fill_in "Country", :with => ""
+    fill_in "Gender", :with => ""
+    click_button "Create Profile"
+    expect(page).to have_content "Your profile was not saved!"
+  end
+
+
+  it "will return an error when the wrong user tries to access another profile" do
+    user = FactoryGirl.create(:user)
+    profile = FactoryGirl.create(:profile, user: user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+
+    user2 = FactoryGirl.create(:user, email: "bad_guy@bad-guy-place.com")
+    profile2 = FactoryGirl.create(:profile, user: user2)
+
+    visit profile_path(profile2.id)
+    expect(page).to have_content "You lie John Snow"
+
+  end
 end
